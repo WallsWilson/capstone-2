52d65c7e7c839dca7a4f84dbc9dfcc96b63caff4
Jordan_Walls-Wilson <jmww1998@gmail.com>
2023-05-21 13:37:53 -0700

    Schema submit

---
+++ README.md
@@ -0,0 +1,3 @@
+- https://admin.timekit.io/a/apps/test-jordan-walls-wilson-8932/resources
+
+- This may change if I end up not liking the way this functions. 
---
+++ body_shop_diagram.sql
@@ -0,0 +1,32 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/lm5ZNw
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Customer" (
+    "cust_id"  SERIAL  NOT NULL,
+    "first_name" string   NOT NULL,
+    "last_name" string   NOT NULL,
+    CONSTRAINT "pk_Customer" PRIMARY KEY (
+        "cust_id"
+     )
+);
+
+CREATE TABLE "Car" (
+    "car_id"  SERIAL  NOT NULL,
+    "car_year" integer   NOT NULL,
+    "car_make" string   NOT NULL,
+    "car_model" string   NOT NULL,
+    "drivable" boolean   NOT NULL,
+    CONSTRAINT "pk_Car" PRIMARY KEY (
+        "car_id"
+     )
+);
+
+CREATE TABLE "Appt" (
+    "date_and_time" timestamp   NOT NULL
+);
+
